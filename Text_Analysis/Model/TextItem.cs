939929ebe31701b623b755a086c7917d258a80ef
lizik <lizik@ELIZA>
2026-02-22 16:08:01 +0300

    fix reading from json

--- Text_Analysis/Model/TextItem.cs
+++ Text_Analysis/Model/TextItem.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Text_Analysis.Model
 {
     public class TextItem
     {
+        [JsonPropertyName("title")]
+
         public string Title { get; set; }
+        [JsonPropertyName("content")]
+
         public string Content { get; set; }
     }
 }
